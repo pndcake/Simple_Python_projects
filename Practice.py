95e1729fdb475e554068f9b3262d6d1834a2bac2
Jorge <jhr1993@gmail.com>
2023-05-12 17:53:54 -0500

    modified guessNumber game

--- Practice.py
+++
@@ -1,4 +0,0 @@
-
-counter = 1
-counter += 1
-print(counter)
--- guessNumber.py
+++ guessNumber.py
@@ -1,16 +1,19 @@
 import random
 num = 10
-def guess(x):
+def guessGame(x):
     random_number = random.randint(1, x)
     guess = 0
-    while guess != random_number or guess.isdigit():
-        guess = int(input(f"Guess a number between 1 and {x}: "))
-        if guess < random_number:
-            print("Sorry, guess again. Too low.")
-        elif guess > random_number:
-            print("Sorry, guess again. Too high.")
+    while guess != random_number:
+        try:
+            guess = int(input(f"Guess a number between 1 and {x}: "))
+            if guess == random_number:
+                print(f"Yay, congrats. You have guessed the number {random_number} correctly!")
+            elif guess < random_number:
+                print("Sorry guess again. Too low.")
+            elif guess > random_number:
+                print("Sorry guess again. Number too high")
+        except ValueError:
+            print("Please input valid number")
         
-            
-    print(f"Yay, congrats. You have guessed the number {random_number} correctly!")
-
-num.isdigit()
+        
+guessGame(100)
