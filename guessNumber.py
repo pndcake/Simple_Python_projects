3105aaa548b4be4782642d958fac525b9a8d29d8
Jorge <jhr1993@gmail.com>
2023-05-15 18:40:50 -0500

    updated with new features

--- guessNumber.py
+++ guessNumber.py
@@ -3,17 +3,22 @@ num = 10
 def guessGame(x):
     random_number = random.randint(1, x)
     guess = 0
-    while guess != random_number:
+    counter = 10
+    while guess != random_number and counter > 0:
         try:
-            guess = int(input(f"Guess a number between 1 and {x}: "))
+            guess = int(input(f" You have {counter} trys, guess the number between 1 and {x}: "))
             if guess == random_number:
                 print(f"Yay, congrats. You have guessed the number {random_number} correctly!")
             elif guess < random_number:
                 print("Sorry guess again. Too low.")
             elif guess > random_number:
                 print("Sorry guess again. Number too high")
+            counter -= 1
         except ValueError:
             print("Please input valid number")
+    if guess != random_number:
+        print("GAME OVER")
+
         
         
 guessGame(100)
